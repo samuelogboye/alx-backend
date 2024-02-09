ee7c0fdf0cba45eaa0ff1d7bcb4e114f3cd1a538
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-09 23:04:16 +0100

    flask initial update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-        return render_template('0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
